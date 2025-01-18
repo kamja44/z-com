05d9cd67e08e15cde922fe312a88f648239f9944
kamja44 <88872409+kamja44@users.noreply.github.com>
2025-01-18 16:22:42 +0900

    next-auth login

---
+++ src/auth.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+} = NextAuth({
+  pages: {
+    signIn: "/i/flow/login",
+    newUser: "/i/flow/signup",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials: Partial<Record<string, any>>): Promise<any> {
+        const authResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: credentials?.username,
+              password: credentials?.password,
+            }),
+          }
+        );
+        if (!authResponse.ok) {
+          return null;
+        }
+        const user = await authResponse.json(); // 로그인 한 사람의 유저정보
+        console.log("user", user);
+        return {
+          email: user.id,
+          name: user.nickname,
+          image: user.image,
+          ...user,
+        };
+      },
+    }),
+  ],
+});
---
+++ src/middleware.ts
@@ -0,0 +1,5 @@
+export { auth as middleware } from "./auth";
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/compose/tweet", "/home", "/explore", "/messages", "/search"],
+};
